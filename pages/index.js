e9027b51aa7193aca37b28b8f2c4e83bcd6942c0
Joshua Cahill <cahillcahillcahill@gmail.com>
2020-11-14 14:28:41 -0600

    first commit, coming soon page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,24 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Thayer Glass Studio</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500&family=Poppins:ital,wght@0,200;0,300;0,400;0,500;1,200&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title="Jon Thayer Stained Glass Studio" />
       </main>
-
       <Footer />
     </div>
-  )
+  );
 }
