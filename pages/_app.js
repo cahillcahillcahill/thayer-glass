a1f0c3f5575ef13386dc7c8a1e0a51d1915ba304
Joshua Cahill <cahillcahillcahill@gmail.com>
2021-01-11 12:21:32 -0600

    image optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
 import "@styles/globals.css";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+
+* {
+  box-sizing: border-box;
+}
+`;
+
+const theme = {
+  // colors: {
+  //   primary: "#0070f3",
+  // },
+};
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default Application;
